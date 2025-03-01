c463b15f7a23732af55a80564559ea051d87e73f
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-03-01 10:58:36 -0300

    chore: initialize api and db service

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask
+from db import db
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://myuser:mypassword@db/core"
 
-app.run(
-    host="127.0.0.1",
-    port=5000
-)
+db.init_app(app)
+
+if __name__ == "__main__":
+  app.run(
+      host="127.0.0.1",
+      port=5000
+  )
