bbaa4f38be2383013928d0641529a9a0bc65b12a
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-03-05 23:52:20 -0300

    test: adjusted conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 
 from app import initialize_app
+from db import db
 
 @pytest.fixture()
 def app():
@@ -8,10 +9,18 @@ def app():
 
     app.config.update({
         "TESTING": True,
-        "SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:"
+        "SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:",
+        "SQLALCHEMY_TRACK_MODIFICATIONS": False
     })
 
+    with app.app_context():
+        db.create_all()
+
     yield app
+
+    with app.app_context():
+        db.session.remove()
+        db.drop_all()
 
 @pytest.fixture()
 def client(app):
